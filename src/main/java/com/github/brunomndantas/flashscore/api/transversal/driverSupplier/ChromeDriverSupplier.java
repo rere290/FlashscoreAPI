1437ca249a19ec72c31ad20fdbb72c25c4ae2ff0
rere290 <71527332+rere290@users.noreply.github.com>
2026-08-26 06:47:31 +0200

    Update ChromeDriverSupplier.java

--- src/main/java/com/github/brunomndantas/flashscore/api/transversal/driverSupplier/ChromeDriverSupplier.java
+++ src/main/java/com/github/brunomndantas/flashscore/api/transversal/driverSupplier/ChromeDriverSupplier.java
@@ -12,34 +12,58 @@ public class ChromeDriverSupplier extends DriverSupplier {
     protected boolean silent;
     protected String driverPath;
 
-
     public ChromeDriverSupplier(String driverPath, boolean silent, boolean headless) {
         this.driverPath = driverPath;
         this.silent = silent;
         this.headless = headless;
     }
 
-
     @Override
     public WebDriver getDriver() throws Exception {
+
         ChromeOptions options = new ChromeOptions();
-        String path = Utils.getAbsolutePath(this.driverPath);
-        if (this.driverPath != null) {
+
+        if (this.driverPath != null && !this.driverPath.isBlank()) {
+            String path = Utils.getAbsolutePath(this.driverPath);
             System.setProperty("webdriver.chrome.driver", path);
         }
 
-        System.setProperty("webdriver.chrome.silentOutput", Boolean.toString(this.silent));
+        System.setProperty(
+                "webdriver.chrome.silentOutput",
+                Boolean.toString(this.silent)
+        );
 
+        /*
+         * Configuration adaptée à un serveur Linux / conteneur.
+         */
         if (this.headless) {
             options.addArguments("--headless=new");
         }
 
-        options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-software-rasterizer");
+
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-background-networking");
+        options.addArguments("--disable-background-timer-throttling");
+        options.addArguments("--disable-backgrounding-occluded-windows");
+        options.addArguments("--disable-renderer-backgrounding");
+
+        options.addArguments("--no-first-run");
+        options.addArguments("--no-default-browser-check");
+        options.addArguments("--disable-popup-blocking");
+
+        options.addArguments("--window-size=1920,1080");
+
         options.addArguments("--remote-allow-origins=*");
+
+        /*
+         * Réduit la consommation mémoire de Chrome.
+         */
+        options.addArguments("--disable-features=Translate,BackForwardCache");
 
         return new ChromeDriver(options);
     }
-
 }
