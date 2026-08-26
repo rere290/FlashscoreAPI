e4ab2f2c33de497b9c60cd953b16647692fde27d
rere290 <71527332+rere290@users.noreply.github.com>
2026-08-26 09:26:12 +0200

    Add DriverSupplierException class for error handling

---
+++ src/main/java/com/github/brunomndantas/flashscore/api/transversal/driverSupplier/DriverSupplierException.java
@@ -0,0 +1,17 @@
+package com.github.brunomndantas.flashscore.api.transversal.driverSupplier;
+
+public class DriverSupplierException extends Exception {
+
+    public DriverSupplierException() {
+        super();
+    }
+
+    public DriverSupplierException(String message) {
+        super(message);
+    }
+
+    public DriverSupplierException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
